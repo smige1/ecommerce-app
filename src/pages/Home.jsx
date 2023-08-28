055ee0a930f5ff54ff3f05c503956147901b77c9
smige1 <egimsikana@gmail.com>
2023-08-28 18:32:56 +0100

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -204,4 +204,32 @@ const Home = () => {
                 </div>
                 <div className="mx-4 w-25 ">
                   <img src="images/brand-02.png" alt="brand" />
-               
+                </div>
+                <div className="mx-4 w-25 ">
+                  <img src="images/brand-03.png" alt="brand" />
+                </div>
+                <div className="mx-4 w-25 ">
+                  <img src="images/brand-04.png" alt="brand" />
+                </div>
+                <div className="mx-4 w-25 ">
+                  <img src="images/brand-05.png" alt="brand" />
+                </div>
+                <div className="mx-4 w-25 ">
+                  <img src="images/brand-06.png" alt="brand" />
+                </div>
+                <div className="mx-4 w-25 ">
+                  <img src="images/brand-07.png" alt="brand" />
+                </div>
+                <div className="mx-4 w-25 ">
+                  <img src="images/brand-08.png" alt="brand" />
+                </div>
+              </Marquee>
+            </div>
+          </div>
+        </div>
+      </section>
+       </>
+  );
+};
+
+export default Home;

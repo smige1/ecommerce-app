b71390918e10ee8debb20d9308e542722b5e0b12
smige1 <egimsikana@gmail.com>
2023-08-28 19:02:34 +0100

    blog uodated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Marquee from "react-fast-marquee";
 import { Link } from "react-router-dom";
-// import BlogCard from "../components/BlogCard";
+import BlogCard from "../components/BlogCard";
 import ProductCard from "../components/ProductCard";
 
 const Home = () => {
@@ -241,7 +241,7 @@ const Home = () => {
           </div>
         </div>
       </section>
-      {/* <section className="blog-wrapper py-5 home-wrapper-2">
+      <section className="blog-wrapper py-5 home-wrapper-2">
         <div className="container-xxl">
           <div className="row">
             <div className="col-12">
@@ -253,7 +253,7 @@ const Home = () => {
             <BlogCard />
           </div>
         </div>
-      </section> */}
+      </section>
        </>
   );
 };

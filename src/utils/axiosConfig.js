19534b1a95ce7de2484fd9d0b7fc9b972833b3d1
smige1 <egimsikana@gmail.com>
2023-09-28 22:26:38 +0100

    updated

--- src/utils/axiosConfig.js
+++ src/utils/axiosConfig.js
@@ -1,3 +1,3 @@
 // export const base_url = "http://localhost:5000/api/"
 
-export const base_url = "https://profound-profiterole-6ca822.netlify.app/api/"
+export const base_url = "https://ecommerce-app-3x6v.vercel.app/api/"

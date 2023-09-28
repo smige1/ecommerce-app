e17b0c473e8bca677cada8499cb91da581035054
smige1 <egimsikana@gmail.com>
2023-09-28 22:16:45 +0100

    updated

--- src/utils/axiosConfig.js
+++ src/utils/axiosConfig.js
@@ -1,1 +1,3 @@
-export const base_url = "http://localhost:5000/api/"
+// export const base_url = "http://localhost:5000/api/"
+
+export const base_url = "https://profound-profiterole-6ca822.netlify.app/api/"
